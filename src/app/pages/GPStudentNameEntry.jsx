903191496b23d3673369f99db55dfc225d0082d3
ultimate365 <maidul365@gmail.com>
2025-12-13 18:46:20 +0530

    Updated Footer

--- src/app/pages/GPStudentNameEntry.jsx
+++ src/app/pages/GPStudentNameEntry.jsx
@@ -1032,67 +1032,6 @@ const GPStudentNameEntry = () => {
               {inputField.name && (
                 <React.Fragment>
                   <div className="mb-3 col-md-3">
-                    <label className="form-label">Gurdian's Name *</label>
-                    <input
-                      type="text"
-                      className="form-control"
-                      placeholder="Gurdian's Name"
-                      value={inputField.gurdiansName}
-                      onChange={(e) => {
-                        setInputField({
-                          ...inputField,
-                          gurdiansName: e.target.value.toUpperCase(),
-                        });
-                      }}
-                      required
-                    />
-                  </div>
-                  <div className="mb-3 col-md-3">
-                    <label className="form-label">Gender *</label>
-                    <select
-                      className="form-select"
-                      id="gender"
-                      defaultValue={""}
-                      onChange={(e) => {
-                        setInpGrSelected(false);
-                        setFirstEventSelected(false);
-                        if (group) {
-                          group.value = "";
-                        }
-                        if (event1) {
-                          event1.value = "";
-                          setInputField({
-                            ...inputField,
-                            event1: "",
-                            event1rank: "",
-                            event2: "",
-                            event2rank: "",
-                          });
-                        }
-
-                        if (event2) {
-                          event2.value = "";
-                          setInputField({
-                            ...inputField,
-                            event1: "",
-                            event1rank: "",
-                            event2: "",
-                            event2rank: "",
-                          });
-                        }
-                        setInputField({
-                          ...inputField,
-                          gender: e.target.value,
-                        });
-                      }}
-                      aria-label="Default select example"
-                    >
-                      <option value="">Select Gender</option>
-                      <option value="BOYS">BOYS</option>
-                      <option value="GIRLS">GIRLS</option>
-                    </select>
-                  </div>
-                  <div className="mb-3 col-md-3">
                     <label className="form-label">BSP Student ID</label>
                     <input
                       type="text"
@@ -1106,6 +1045,22 @@ const GPStudentNameEntry = () => {
                         });
                       }}
                       maxLength={14}
+                      required
+                    />
+                  </div>
+                  <div className="mb-3 col-md-3">
+                    <label className="form-label">Gurdian's Name *</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="Gurdian's Name"
+                      value={inputField.gurdiansName}
+                      onChange={(e) => {
+                        setInputField({
+                          ...inputField,
+                          gurdiansName: e.target.value.toUpperCase(),
+                        });
+                      }}
                       required
                     />
                   </div>
@@ -1179,6 +1134,52 @@ const GPStudentNameEntry = () => {
                       })}
                     </select>
                   </div>
+                  <div className="mb-3 col-md-3">
+                    <label className="form-label">Gender *</label>
+                    <select
+                      className="form-select"
+                      id="gender"
+                      defaultValue={""}
+                      onChange={(e) => {
+                        setInpGrSelected(false);
+                        setFirstEventSelected(false);
+                        if (group) {
+                          group.value = "";
+                        }
+                        if (event1) {
+                          event1.value = "";
+                          setInputField({
+                            ...inputField,
+                            event1: "",
+                            event1rank: "",
+                            event2: "",
+                            event2rank: "",
+                          });
+                        }
+
+                        if (event2) {
+                          event2.value = "";
+                          setInputField({
+                            ...inputField,
+                            event1: "",
+                            event1rank: "",
+                            event2: "",
+                            event2rank: "",
+                          });
+                        }
+                        setInputField({
+                          ...inputField,
+                          gender: e.target.value,
+                        });
+                      }}
+                      aria-label="Default select example"
+                    >
+                      <option value="">Select Gender</option>
+                      <option value="BOYS">BOYS</option>
+                      <option value="GIRLS">GIRLS</option>
+                    </select>
+                  </div>
+
                   <div className="mb-3 col-md-3">
                     <label className="form-label">Select Group *</label>
                     <select
