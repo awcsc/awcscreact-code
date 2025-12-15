07cb967892606b7feed57e8268909eada13de980
ultimate365 <maidul365@gmail.com>
2025-12-15 19:19:53 +0530

    Updated Footer

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -252,7 +252,10 @@ const Navbar = () => {
         to: "/CircleSportsDirectNameEntry",
         show: (ctx) =>
           isTeacher(ctx) &&
-          (hasAccess(ctx, "admin") || userHas(ctx, "circleAssistant", "admin")),
+          (hasAccess(ctx, "admin") ||
+            userHas(ctx, "convenor", "admin") ||
+            userHas(ctx, "gpAssistant", "admin") ||
+            userHas(ctx, "circleAssistant", "admin")),
       },
     ];
 
